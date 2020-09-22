37e897d67e76668e87aeefd2e65a3cf53a581ff6
ahmedelmoselhy98 <ahmedelmoselhy98@gmail.com>
2020-09-22 13:36:21 +0200

    #ah

---
+++ app/src/main/java/com/refFamily/activities_fragments/activity_order_steps/OrderStepsActivity.java
@@ -0,0 +1,16 @@
+package com.refFamily.activities_fragments.activity_order_steps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.refFamily.R;
+
+public class OrderStepsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_steps);
+    }
+}
